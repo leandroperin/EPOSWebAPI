6d391a6eb3a436100cee5b934c75ad40949d4f1b
Leandro Perin <leandro.perino@gmail.com>
2018-07-03 00:57:10 -0300

    Start/Stop added

--- public/engine.js
+++ public/engine.js
@@ -1,11 +1,27 @@
+var is_on = false;
+
 $(document).ready(function() {
   setInterval(function() {
     refresh()
   }, 100);
 });
 
+$('#btnSS').click(function() {
+  if (is_on) {
+    is_on = false;
+    $("#btnSS").html("Start");
+    console.log("Information getting stopped");
+  } else {
+    is_on = true;
+    $("#btnSS").html("Stop");
+    console.log("Information getting started");
+  }
+});
+
 function refresh() {
-  $.getJSON('http://localhost:3000/gett', function(data) {
-    $('#currTemp').html(data['temp'] + ' ºC');
-  });
+  if (is_on) {
+    $.getJSON('http://localhost:3000/gett', function(data) {
+      $('#currTemp').html(data['temp'] + ' ºC');
+    });
+  }
 };
